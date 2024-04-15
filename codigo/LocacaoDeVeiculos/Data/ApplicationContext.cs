76f804f1acb2e269905473afdd9d584b29bc1ef1
Davi Ribeiro <davirpa1@gmail.com>
2024-04-14 21:42:15 -0300

    criação do contexto de banco de dados do app

---
+++ codigo/LocacaoDeVeiculos/Data/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using LocacaoDeVeiculos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocacaoDeVeiculos.Data
+
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+    }
+}
