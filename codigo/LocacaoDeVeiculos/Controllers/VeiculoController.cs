57cf3504a119c523331cd82d6ebb2a37ba88a078
Davi Ribeiro <davirpa1@gmail.com>
2024-04-18 20:46:31 -0300

    add: classe VeiculoController

---
+++ codigo/LocacaoDeVeiculos/Controllers/VeiculoController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LocacaoDeVeiculos.Data;
+using LocacaoDeVeiculos.Models;
+
+namespace LocacaoDeVeiculos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculoController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public VeiculoController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Veiculos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Veiculo>>> GetVeiculos()
+        {
+            return await _context.Veiculos.ToListAsync();
+        }
+
+        // GET: api/Veiculos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Veiculo>> GetVeiculo(int id)
+        {
+            var veiculo = await _context.Veiculos.FindAsync(id);
+
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            return veiculo;
+        }
+
+        // PUT: api/Veiculos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVeiculo(int id, Veiculo veiculo)
+        {
+            if (id != veiculo.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(veiculo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VeiculoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Veiculos
+        [HttpPost]
+        public async Task<ActionResult<Veiculo>> PostVeiculo(Veiculo veiculo)
+        {
+            _context.Veiculos.Add(veiculo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVeiculo", new { id = veiculo.ID }, veiculo);
+        }
+
+        // DELETE: api/Veiculos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVeiculo(int id)
+        {
+            var veiculo = await _context.Veiculos.FindAsync(id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Veiculos.Remove(veiculo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VeiculoExists(int id)
+        {
+            return _context.Veiculos.Any(e => e.ID == id);
+        }
+    }
+}
